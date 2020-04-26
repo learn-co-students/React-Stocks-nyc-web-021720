1b35aec5862d3e80ee32b4cf65aa284ec4a0f01a
osgood gunawan <ogunawan@ucsd.edu>
2020-04-26 15:53:47 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import Stock from '../components/Stock'
+import Stock from '../components/Stock';
+import PropTypes from "prop-types";
 
 class PortfolioContainer extends Component {
 
   render() {
+    const {stocks,removeStock}=this.props
+
     return (
       <div>
         <h2>My Portfolio</h2>
           {
             //render your portfolio stocks here
+            stocks.map(stock => <Stock key={stock.id} stock={stock} removeStock={removeStock}/>)
+            
           }
       </div>
     );
   }
 
+}
+
+PortfolioContainer.propTypes={
+  stocks: PropTypes.array.isRequired,
+  removeStock: PropTypes.func.isRequired
 }
 
 export default PortfolioContainer;
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import Stock from '../components/Stock'
+import PropTypes from 'prop-types'
 
 class StockContainer extends Component {
 
   render() {
+
+    const {stocks,addPortfolio}=this.props
+    // console.log(stocks)
     return (
       <div>
         <h2>Stocks</h2>
         {
           //render the list of stocks here
+          stocks.map(stock =><Stock key={stock.id} stock={stock} addPortfolio={addPortfolio}/>)
         }
       </div>
     );
   }
 
+}
+
+StockContainer.propTypes={
+stocks :PropTypes.array.isRequired,
+addPortfolio :PropTypes.func.isRequired
 }
 
 export default StockContainer;
